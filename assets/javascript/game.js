d6f96f2c3964b1f6a5ba6ee9e516e508bf4cb564
burritoscott <36803614+burritoscott@users.noreply.github.com>
2018-06-03 23:00:21 -0700

    created functions for letters S, P, and C and wrote down notes in comments for further work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,25 +9,58 @@
 
 
 
-// array containing all of the letters of the alphabed
+// // array containing all of the letters of the alphabed
 var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 
-// a for loop to cycle through the alphabet array
+// // a for loop to cycle through the alphabet array (works in consol!)
 for ( var i = 0; i < alphabet.length; i++ ){
     console.log(alphabet[i]);
 }
 
 
 // function to convert the users letter to uppercase
-// and an if else to check to see if the answer is true or false
-function guess(x){
+// and an if else to check to see if the answer is "P" (works in console!)
+function guessP(x){
     var x = x.toUpperCase();
-    if (x == true){
+    if (x == "P"){
         console.log("works");
     }else{
         console.log("doesn't work");
     }
 }
 
-guess("M");
+// function to convert the users letter to uppercase
+// and an if else to check to see if the answer is "C" (works in console!)
+function guessC(x){
+    var x = x.toUpperCase();
+    if (x == "C"){
+        console.log("works");
+    }else{
+        console.log("doesn't work");
+    }
+}
+
+// function to convert the users letter to uppercase
+// and an if else to check to see if the answer is "S" (works in console!)
+function guessS(x){
+    var x = x.toUpperCase();
+    if (x == "S"){
+        console.log("works");
+    }else{
+        console.log("doesn't work");
+    }
+}
+
+// write a function to keep track or letters guessed
+// write a function to keep track of number of guesses remaining
+// write a function that tracks wins (if letter is guessed increase the value of var score by 1)
+// write a function that tracks losses
+// create a variable called 'score' that increases with each win.
+// create a variable called 'losses' that increases with each lose.
+
+
+
+
+
+guessP("p");
