26dda4a33f2f759e4639d2e4950709748c3f661f
burritoscott <36803614+burritoscott@users.noreply.github.com>
2018-06-07 21:42:51 -0700

    completed psychic game but cant seem to have guesses not be replaced with next guess   may have to create an empty array for guesses to add to the array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,28 +7,41 @@
 //create three things to guess:  The Legend of Zelda,  Metroid, Mario Bros
 //find sound bites for guesses and wins & loses
 
-// LOOK AT WEEK 4 DAY 3 ACTIVITY 4 TO DO THIS ASSIGNMENT
-
-
 // // array containing all of the letters of the alphabed
-var alphabet = ["S","C","O","T","C","H","A"];
+var alphabet = ["s","c","o","t","h","a"];
 
 var wins = 0;
 var losses = 0;
 var numGuessLeft = 9;
-var guesses = "i";
-
+var guesses = [];
 
 function playGame(event){
-    var computerOptionIndex = Math.floor(Math.random())*alphabet.length;
+    var computerOptionIndex = Math.floor(Math.random()*alphabet.length);
 
     var computerLetter = alphabet[computerOptionIndex];
     var userChoice = event.key;
-
+    var guess = "";
     document.querySelector("#psychicLetter").innerText = computerLetter;
     document.querySelector("#lettersGuessed").innerText = userChoice;
+
+    if (userChoice == computerLetter){
+        wins++;
+        document.querySelector("#wins").innerText = wins;
+        numGuessLeft = 9;
+        document.querySelector("#numGuess").innerText = numGuessLeft;
+        document.querySelector("#lettersGuessed").innerText = guesses;
+    }else{
+        numGuessLeft--;
+        document.querySelector("#numGuess").innerText = numGuessLeft;
+            if (numGuessLeft == 0){
+            losses++;
+            document.querySelector("#loses").innerText = losses;
+            numGuessLeft = 9;
+            document.querySelector("#numGuess").innerText = numGuessLeft;
+            }
+    } 
 }
-    document.onkeyup = playGame;
+document.onkeyup = playGame;
 
 
 
