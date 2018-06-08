1b91bece62e8961f915abe39dc771be946b67d47
Burrito Scott <scottchapman@Scotts-MacBook-Pro.local>
2018-06-07 20:17:47 -0700

    letters guessed follows key presses and computer guesses a letter and reveals it after one guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,60 +7,28 @@
 //create three things to guess:  The Legend of Zelda,  Metroid, Mario Bros
 //find sound bites for guesses and wins & loses
 
+// LOOK AT WEEK 4 DAY 3 ACTIVITY 4 TO DO THIS ASSIGNMENT
 
 
 // // array containing all of the letters of the alphabed
-var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var alphabet = ["S","C","O","T","C","H","A"];
+
+var wins = 0;
+var losses = 0;
+var numGuessLeft = 9;
+var guesses = "i";
 
 
-// // a for loop to cycle through the alphabet array (works in consol!)
-for ( var i = 0; i < alphabet.length; i++ ){
-    console.log(alphabet[i]);
+function playGame(event){
+    var computerOptionIndex = Math.floor(Math.random())*alphabet.length;
+
+    var computerLetter = alphabet[computerOptionIndex];
+    var userChoice = event.key;
+
+    document.querySelector("#psychicLetter").innerText = computerLetter;
+    document.querySelector("#lettersGuessed").innerText = userChoice;
 }
-
-
-// function to convert the users letter to uppercase
-// and an if else to check to see if the answer is "P" (works in console!)
-function guessP(x){
-    var x = x.toUpperCase();
-    if (x == "P"){
-        console.log("works");
-    }else{
-        console.log("doesn't work");
-    }
-}
-
-// function to convert the users letter to uppercase
-// and an if else to check to see if the answer is "C" (works in console!)
-function guessC(x){
-    var x = x.toUpperCase();
-    if (x == "C"){
-        console.log("works");
-    }else{
-        console.log("doesn't work");
-    }
-}
-
-// function to convert the users letter to uppercase
-// and an if else to check to see if the answer is "S" (works in console!)
-function guessS(x){
-    var x = x.toUpperCase();
-    if (x == "S"){
-        console.log("works");
-    }else{
-        console.log("doesn't work");
-    }
-}
-
-// write a function to keep track or letters guessed
-// write a function to keep track of number of guesses remaining
-// write a function that tracks wins (if letter is guessed increase the value of var score by 1)
-// write a function that tracks losses
-// create a variable called 'score' that increases with each win.
-// create a variable called 'losses' that increases with each lose.
+    document.onkeyup = playGame;
 
 
 
-
-
-guessP("p");
